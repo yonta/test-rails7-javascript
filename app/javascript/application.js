51d745338a958cd5ffb6f334785b2039070877ac
SAITOU Keita <keita44.f4@gmail.com>
2022-03-03 01:03:39 +0900

    Add bootstrap style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "@popperjs/core"
+import "bootstrap"
+
+var popoverTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
