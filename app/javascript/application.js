a8a0ea34a942a688ed42efe6c4b7747bbccca691
SAITOU Keita <keita44.f4@gmail.com>
2022-03-04 02:11:05 +0900

    Add bootstrap
    yarn add bootstrap
    edit application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Tooltip, Popover } from "bootstrap"
+
+var popoverTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+  return new Popover(popoverTriggerEl)
+})
+
+var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+  return new Tooltip(tooltipTriggerEl)
+})
